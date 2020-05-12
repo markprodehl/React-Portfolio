e5fa072b087a0a0b4c3f3e94e2c99a3077c13de8
Mark Prodehl <markprodehl@gmail.com>
2020-05-12 00:47:50 -0700

    added about me and navbar. Started routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
+import NavBar from './components/NavBar';
 
 function App() {
-  return <AboutMe />;
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        
+      </div>
+    </Router>
+  );
 }
 
 export default App;
