e5b27d7ee0126c7ce9a92c9e5ee8a667b56f2cd8
Mark Prodehl <markprodehl@gmail.com>
2020-05-14 00:17:33 -0700

    Target_blank added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ function Footer(props) {
     return (
       
         <div className="d-flex justify-content-center mt-5 container fixed-bottom">
-            <a className="socialLinks text-center" href="https://www.linkedin.com/in/mark-prodehl-95a04433/"><i className="fab fa-linkedin fa-3x"></i></a>
-            <a className="socialLinks text-center" href="https://github.com/markprodehl"><i className="fab fa-github fa-3x"></i></a>
-            <a className="socialLinks text-center" href="https://1drv.ms/b/s!AvkX4tPbiTsRiONeA6zu9IHohkgmpQ"><i className="far fa-file fa-3x"></i></a>
+            <a className="socialLinks text-center" href="https://www.linkedin.com/in/mark-prodehl-95a04433/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-3x"></i></a>
+            <a className="socialLinks text-center" href="https://github.com/markprodehl" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-3x"></i></a>
+            <a className="socialLinks text-center" href="https://1drv.ms/b/s!AvkX4tPbiTsRiONeA6zu9IHohkgmpQ" target="_blank" rel="noopener noreferrer"><i className="far fa-file fa-3x"></i></a>
         </div>
     )
 }
--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -22,7 +22,7 @@ function ProjectPage() {
                             <p >30-Day-Challenge is a full-stack web application that uses server side-API and an MVC design pattern. The application is designed for people that want to challenge themselves by committing to a fitness regime designed to challenge their physical and mental toughness. Users set can set multiple 30-day goals which they can keep track of in the easy to use challenge dashboard. 
                             </p>
                             <div className="d-flex justify-content-center mt-4">
-                                <a className="myBtn text-center" href="https://thirty-day-challenge.herokuapp.com/login">VIEW APP</a>
+                                <a className="myBtn text-center" href="https://thirty-day-challenge.herokuapp.com/login" target="_blank" rel="noopener noreferrer">VIEW APP</a>
                             </div>
                         </div>
                     </div>
@@ -36,7 +36,7 @@ function ProjectPage() {
                             <p>The Food Logger application uses MySQL, Node, Express, Handlebars and a custom built ORM designed to take customer orders and keep a record of the ones that have been delivered and eaten. Devoured burgers are kept in a database for the restaurant managers records.
                             </p>
                             <div className="d-flex justify-content-center mt-4">
-                                <a className="myBtn text-center" href="https://the-food-logger.herokuapp.com/">VIEW APP</a>
+                                <a className="myBtn text-center" href="https://the-food-logger.herokuapp.com/" target="_blank" rel="noopener noreferrer">VIEW APP</a>
                             </div>
                         </div>
                         <div className="col-lg-6">
@@ -56,7 +56,7 @@ function ProjectPage() {
                             <p>The Online-Offline-Budget-Tracker App is designed to optimize user experience, particularly on older phones with slower internet connections. It is a progressive web application that uses a Service Worker and Cache API’s to cache assets and API responses. This means that it can function without an internet connection.
                             </p>
                             <div className="d-flex justify-content-center mt-4">
-                                <a className="myBtn text-center" href="https://offlinebudget-tracker.herokuapp.com/">VIEW APP</a>
+                                <a className="myBtn text-center" href="https://offlinebudget-tracker.herokuapp.com/" target="_blank" rel="noopener noreferrer">VIEW APP</a>
                             </div>
                         </div>
                     </div>
@@ -70,7 +70,7 @@ function ProjectPage() {
                             <p>Memory Game is an application designed to work in the browser. No need to buy or install anything locally. This makes it a cheap and convenient way to exercise your memory while having a fun. Memory Game can be played by people of all ages.
                             </p>
                             <div className="d-flex justify-content-center mt-4">
-                                <a className="myBtn text-center" href="https://markprodehl.github.io/Memory-Game/">VIEW APP</a>
+                                <a className="myBtn text-center" href="https://markprodehl.github.io/Memory-Game/" target="_blank" rel="noopener noreferrer">VIEW APP</a>
                             </div>
                         </div>
                         <div className="col-lg-6">
