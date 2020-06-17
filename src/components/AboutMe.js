9df26489bd639900f638ff5329556a55d846b5eb
Mark Prodehl <markprodehl@gmail.com>
2020-06-17 13:32:42 -0700

    Updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,9 +16,9 @@ function AboutMe(props) {
               </div>
               <div className="col-lg-7 mt-1">
                   <h2 className="pinkTxt" >About Me</h2>
-                  <p className="">Full Stack Web Developer with a passion for creativity in development and excited to contribute to the computer science field. Patient in developing solutions by tackling problems in an exceptionally detail-oriented and methodical nature. Performs well under the pressure of organizing and upholding project deadlines by being flexible and dependable. Currently working towards a Full Stack Coding Bootcamp Certificate from UCSD.</p>
-                  <p className="">An experienced Operations and Business Development Manager with proven results in growing new revenue streams across multiple sectors. Demonstrated ability at maximizing sales & profitability, growing customer portfolios and developing a broad knowledge across operations, business systems, logistics, and technology.</p>
-                  <p className="">Applying a personable approach to stakeholder relationships, I have built business with new customers and collaborated with existing clients to ensure that their requirements are fully met. Having worked at a senior level, I have expertise in developing bespoke services and delivering on business commitments to achieve excellence.</p>
+                  <p className="">In addition to recently completing a Full Stack Web Developer Certification from the University of California, San Diego, I have over 20 years of experience in business operations, sales, marketing and product management to leverage in the web development field. I have proven results growing revenue streams across multiple business sectors and demonstrable ability at maximizing sales, building customer relationships, growing portfolios, and increasing profitability. I also have a broad knowledge across business operations, systems, logistics, and e-commerce.</p>
+                  <p className="">The web development skills I’ve learned using agile development techniques and Github version control combined with years of experience using web applications in a business environment positions me well to align web application design with the functionality that end users need.</p>
+                  <p className="">I am a self-motivated, adaptable, flexible, and collaborative person who is open to learning on the job. </p>
                   
                   <div className="d-flex justify-content-center mt-4">
                       <Link to={'/ProjectPage'}>
