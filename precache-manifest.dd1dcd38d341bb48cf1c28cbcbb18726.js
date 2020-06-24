ae55a95d61640e2af9f53637531d92bef1abe1e2
Mark Prodehl <markprodehl@gmail.com>
2020-06-24 15:09:33 -0700

    Updates

---
+++ precache-manifest.dd1dcd38d341bb48cf1c28cbcbb18726.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfb812167c95fde248b933f9d744659",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "fe66104fc304026e0354",
+    "url": "/React-Portfolio/static/css/main.a14e80ad.chunk.css"
+  },
+  {
+    "revision": "e01b6caa376d83ba4fe7",
+    "url": "/React-Portfolio/static/js/2.67cafc35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.67cafc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe66104fc304026e0354",
+    "url": "/React-Portfolio/static/js/main.c65de54d.chunk.js"
+  },
+  {
+    "revision": "55f8281e41e63d261000",
+    "url": "/React-Portfolio/static/js/runtime-main.a0d97005.js"
+  },
+  {
+    "revision": "d7686a9338745db0c55d0b9a8e6e944a",
+    "url": "/React-Portfolio/static/media/appScreenshot.d7686a93.png"
+  },
+  {
+    "revision": "7fde8ae3013b1dbe984117b00692a0a3",
+    "url": "/React-Portfolio/static/media/foodLogger.7fde8ae3.png"
+  },
+  {
+    "revision": "6acc6adca588948f1bff8ebea3bcf3a7",
+    "url": "/React-Portfolio/static/media/mapSD.6acc6adc.png"
+  },
+  {
+    "revision": "dedc9802adf498699336f13d46c6b70d",
+    "url": "/React-Portfolio/static/media/memoryGame.dedc9802.png"
+  },
+  {
+    "revision": "34f3f043046828906fce876aa6305060",
+    "url": "/React-Portfolio/static/media/onlineOfflineTracker.34f3f043.png"
+  },
+  {
+    "revision": "f804d31953235011eafb40c97fb9e455",
+    "url": "/React-Portfolio/static/media/profilePicture.f804d319.png"
+  },
+  {
+    "revision": "4f8563d02fb4bd989b90eb509cafb1a7",
+    "url": "/React-Portfolio/static/media/thirtyDays.4f8563d0.png"
+  }
+]);
